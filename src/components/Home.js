869545bce11bf31453a0d3e2af952652cb0e92b7
prash88 <prash88@iastate.edu>
2017-08-11 20:28:41 -0700

    Adding gh pages home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,6 @@ import React, { Component } from 'react';
 import '../css/App.css';
 
 const Home = (): React.Element<*> =>
-  <div><h1>Home</h1></div>;
+  <div><h1>Welcome</h1></div>;
 
 export default Home;
